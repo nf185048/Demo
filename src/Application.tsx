8945926dac91eccc67c9924732c77b5b1d859dc1
Nishat Firoj <nf185048@ncr.com>
2021-06-07 14:36:24 -0400

    Cleaned dev environment

--- src/Application.tsx
+++ src/Application.tsx
@@ -14,56 +14,57 @@ function Application() {
       <Router>
         <List>
           <ListTitle className='list-title'>Web Component Showcase</ListTitle>
-          <Grid container xs={6}>
+          <Grid container xs={12}>
             <ItemGroup>
               <Item>
-                <Label>Controls</Label>
+                <Label position='stacked'>Controls</Label>
                 <Link className='router-link' to='/pages/Buttons'>
                   Buttons
                 </Link>
-                |
                 <Link className='router-link' to='/pages/Checkboxes'>
                   Checkboxes
                 </Link>
               </Item>
+
               <Item>
-                <Label>Containers</Label>
+                <Label position='stacked'>Containers</Label>
                 <Link className='router-link' to='/pages/Modals'>
                   Modals
                 </Link>
-                |
                 <Link className='router-link' to='/pages/Drawers'>
                   Drawers
                 </Link>
               </Item>
+
               <Item>
-                <Label>Content</Label>
+                <Label position='stacked'>Content</Label>
                 <Link className='router-link' to='/pages/Typographies'>
                   Typographies
                 </Link>
-                |
                 <Link className='router-link' to='/pages/Colors'>
                   Colors
                 </Link>
               </Item>
+
               <Item>
-                <Label>Digital Ordering</Label>
-                <Link className='router-link' to='/pages/DigitalOrdering'>
-                  Digital Ordering
+                <Label position='stacked'>Digital Ordering</Label>
+                <Link className='router-link' to='/pages/OrderSummary'>
+                  Order Summary
                 </Link>
               </Item>
+
               <Item>
-                <Label>Layout</Label>
+                <Label position='stacked'>Layout</Label>
                 <Link className='router-link' to='/pages/Lists'>
                   Lists
                 </Link>
-                |
                 <Link className='router-link' to='/pages/Grids'>
                   Grids
                 </Link>
               </Item>
+
               <Item>
-                <Label>App Clones</Label>
+                <Label position='stacked'>App Clones</Label>
                 <Link className='router-link' to='/pages/AppClones'>
                   App Clones
                 </Link>
@@ -83,7 +84,7 @@ function Application() {
             <Route path='/pages/Colors' component={() => <Pages.Colors />} />
             <Route path='/pages/Lists' component={() => <Pages.Lists />} />
             <Route path='/pages/Grids' component={() => <Pages.Grids />} />
-            <Route path='/pages/DigitalOrdering' component={() => <Pages.DigitalOrdering />} />
+            <Route path='/pages/DigitalOrdering' component={() => <Pages.OrderSummary />} />
             <Route path='/pages/AppClones' component={() => <Pages.AppClones />} />
           </Switch>
         </Grid>
---
+++ src/pages/OrderSummary.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { FC } from 'react'
+
+export const OrderSummary: FC<{}> = () => {
+  return (
+    <div>
+      <p>Order Summary</p>
+    </div>
+  )
+}
